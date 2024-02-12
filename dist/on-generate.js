6b0b17025121baf28b01cec79f3cf2815e09fc90
broisnischal <neeswebservice@gmail.com>
2024-02-12 21:17:27 +0545

    feat:added-features-updated

--- dist/on-generate.js
+++ dist/on-generate.js
@@ -7,18 +7,12 @@ const util_1 = require("./util");
 async function onGenerate(options) {
     let exportedTypes = "";
     const dataModel = options.dmmf.datamodel;
-    // our generator code here
     for (const model of dataModel.models) {
         exportedTypes += `export interface ${model.name} {\n`;
-        // Only convert fields with kind "scalar" and "enum"
         const scalarAndEnumFields = model.fields.filter((field) => ["scalar", "enum"].includes(field.kind));
         for (const field of scalarAndEnumFields) {
-            // A utility function to convert Prisma types to TypeScript types
-            // We'll create this function later.
             const typeScriptType = (0, util_1.getTypeScriptType)(field.type);
-            // Whether the field should be optional
             const nullability = field.isRequired ? "" : "| null";
-            // Whether the field should be an array
             const list = field.isList ? "[]" : "";
             exportedTypes += `${field.name}: ${typeScriptType}${nullability}${list};\n`;
         }
@@ -34,14 +28,10 @@ async function onGenerate(options) {
     }
     const outputDir = options.generator.output?.value ?? "./types";
     const fullLocaltion = `${outputDir}/index.ts`;
-    // Make sure the output directory exists, if not create it
     (0, node_fs_1.mkdirSync)(outputDir, { recursive: true });
-    // Format the generated code
     const formattedCode = await (0, prettier_1.format)(exportedTypes, {
-        // ... your preferred prettier options
         parser: "typescript",
     });
-    // Write the formatted code to a file
     (0, node_fs_1.writeFileSync)(fullLocaltion, formattedCode);
 }
 exports.onGenerate = onGenerate;
